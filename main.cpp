2b6084f163385cadb9cf5735b5a7106e30188ab7
albandescottes <alban.descottes@gmail.com>
2017-12-09 15:52:48 +0100

    ajout main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "Point.h"
+#include "Ecran.h"
+#include "Scene.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	Scene *pSc = new Scene();
+	if(1 == pSc->parseFile()){
+		cerr << "shit\n";
+	}
+    cout << *pSc;
+	return 0;
+}
